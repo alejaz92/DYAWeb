291fe043ff4b132a7a4935377246c72cd8e96868
alejaz92 <alejandro.jaz92@gmail.com>
2023-05-23 19:39:25 -0300

    Actividad clase 08

---
+++ Clase08/Exercises/variables_and_operators.js
@@ -0,0 +1,16 @@
+var a = 2;
+var b = 5;
+var c = a + b;
+console.log(c);
+
+/*********** */
+
+var d = "abc";
+var e = "123";
+var f = d + e;
+
+/********* */
+
+var g = "abc";
+var h = "123";
+var i = length(g) + length(h);
